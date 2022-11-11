868166e0fb5b0801b13d45a7c3e9b897ee52e51c
Leila Moraes <109045940+leilaMoraes@users.noreply.github.com>
2022-11-10 22:02:05 -0300

    Conserta lint

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Button from '../components/Button';
 
