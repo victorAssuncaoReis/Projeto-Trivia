442646f155663ab7f61eddb534139348a7143193
Victor <victorreis222@gmail.com>
2022-11-11 17:10:13 -0300

    Primeira parte req 6

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
-import Button from '../components/Button';
+/* import Button from '../components/Button'; */
 
 class Game extends React.Component {
   state = {
-    category: [],
-    rightAnswers: [],
-    wrongAnswers: [],
     question: [],
+    index: 0,
+    loading: false,
   };
 
   componentDidMount() {
@@ -16,6 +15,7 @@ class Game extends React.Component {
   }
 
   getQuestions = async (token) => {
+    const { index } = this.state;
     const url = `https://opentdb.com/api.php?amount=5&token=${token}`;
     const response = await fetch(url);
     const data = await response.json();
@@ -25,39 +25,91 @@ class Game extends React.Component {
       window.location.replace('/');
     } else {
       this.setState({
-        category: questions.map((el) => el.category),
-        rightAnswers: questions.map((el) => el.correct_answer),
-        wrongAnswers: questions.map((el) => el.incorrect_answers),
-        question: questions.map((el) => el.question),
+        question: questions[index],
+        loading: true,
       });
     }
   };
 
   render() {
-    const { category, rightAnswers, wrongAnswers,
-      question } = this.state;
+    const { question, loading } = this.state;
     return (
       <div>
-        <Header />
-        <h3 data-testid="question-category">{category}</h3>
-        <p data-testid="question-text">{question}</p>
-        <div data-testid="answer-options">
-          { rightAnswers.map((el, i) => (
-            <Button
-              key={ i }
-              btnName={ el[0] }
-              dataName="correct-answer"
-            />))}
-          { wrongAnswers.map((el, i) => (
-            <Button
-              key={ i }
-              btnName={ el }
-              dataName={ `wrong-answer-${i}` }
-            />))}
-        </div>
+        { loading
+          ? (
+            <div>
+              <Header />
+              <h3 data-testid="question-category">{ question.category }</h3>
+              <p data-testid="question-text">{ question.question }</p>
+              <div data-testid="answer-options">
+                {/*  { allAnswers.map((el, i) => (
+                  <Button
+                    type="button"
+                    key={ i }
+                    btnName={ el[i] }
+                    dataName="correct-answer"
+                  >
+                    { el[i] }
+                  </Button>))} */}
+              </div>
+            </div>)
+          : <div>{' '}</div> }
       </div>
     );
   }
 }
 
 export default Game;
+
+/*   randomOrder() {
+    const myNum = 0.5;
+    return (Math.round(Math.random()) - myNum);
+  }
+  Criar um state de index?
+
+  const correctAnswer = trivia[index].correct_answer;
+  const wrongAnswers = trivia[index].incorrect_answers[0];
+
+  const renderCorrect = (
+      <button
+        type="button"
+        data-testid="correct-answer"
+        className="correct-answer"
+      >
+        {rightAnswer}
+      </button>);
+
+  const mapWrongAnsers = wrongAnwswers.map((answer, index) => (
+      <button
+        type="button"
+        key={ index }
+        data-testid={ `wrong-answer${index}` }
+      >
+        {answer}
+      </button>
+    ));
+
+  }
+  const allAnswers = [];
+  allAnswers.push(correctAnswer, wrongAnswers);
+
+  allAnswers.sort(this.randomOrder);
+
+  return (
+      <div>
+        <Header />
+        {trivia
+        && (
+          <main>
+            <h1 data-testid="question-category">{trivia.category}</h1>
+            <h2 data-testid="question-text">{trivia.question}</h2>
+            <div data-testid="answer-options">
+              {allAnswers.map((button) => button)}
+            </div>
+          </main>
+        )}
+      </div>
+    );
+  }
+}
+  */
