3c709ad52c0bc9cb7a7ba2b75dc130f17ecdc9f7
Leila Moraes <leila.violino@gmail.com>
2022-11-11 23:40:32 -0300

    Adiciona lógica para mudar a cor do botão das respostas ao ser clicado

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from '../components/Header';
 import Button from '../components/Button';
+import './game.css';
 
 class Game extends React.Component {
   state = {
-    category: [],
-    rightAnswers: [],
-    wrongAnswers: [],
+    index: 0,
+    loading: true,
     question: [],
+    mixAnswers: [],
+    changeClass: false,
   };
 
   componentDidMount() {
@@ -16,48 +18,86 @@ class Game extends React.Component {
   }
 
   getQuestions = async (token) => {
+    const { index } = this.state;
     const url = `https://opentdb.com/api.php?amount=5&token=${token}`;
     const response = await fetch(url);
     const data = await response.json();
     const questions = data.results;
+
     if (questions.length === 0) {
       localStorage.clear('token');
       window.location.replace('/');
     } else {
       this.setState({
-        category: questions.map((el) => el.category),
-        rightAnswers: questions.map((el) => el.correct_answer),
-        wrongAnswers: questions.map((el) => el.incorrect_answers),
-        question: questions.map((el) => el.question),
-      });
+        loading: false,
+        question: questions[index],
+        changeClass: false,
+      }, () => this.shuffleAnswers());
     }
   };
 
+  shuffleAnswers = () => {
+    const { question: {
+      incorrect_answers: incorrectAnswers,
+      correct_answer: correctAnswer } } = this.state;
+    const allAnswers = [...incorrectAnswers];
+    const magicNumber = 5;
+    const randomIndex = Math.floor(Math.random() * magicNumber);
+    allAnswers.splice(randomIndex, 0, correctAnswer);
+    this.setState({ mixAnswers: allAnswers });
+  };
+
+  handleClick = () => {
+    this.setState({ changeClass: true });
+  };
+
   render() {
-    const { category, rightAnswers, wrongAnswers,
-      question } = this.state;
+    const { loading,
+      mixAnswers,
+      changeClass,
+      question: {
+        category,
+        question,
+        correct_answer: correctAnswer,
+      },
+    } = this.state;
+    console.log(mixAnswers);
     return (
-      <div>
+      <>
         <Header />
-        <h3 data-testid="question-category">{category}</h3>
-        <p data-testid="question-text">{question}</p>
-        <div data-testid="answer-options">
-          { rightAnswers.map((el, i) => (
-            <Button
-              key={ i }
-              btnName={ el[0] }
-              dataName="correct-answer"
-            />))}
-          { wrongAnswers.map((el, i) => (
-            <Button
-              key={ i }
-              btnName={ el }
-              dataName={ `wrong-answer-${i}` }
-            />))}
-        </div>
-      </div>
+        { loading
+          ? <h1>Carregando...</h1>
+          : (
+            <>
+              <h3 data-testid="question-category">{category}</h3>
+              <p data-testid="question-text">{question}</p>
+              <div data-testid="answer-options">
+                { mixAnswers.map((el, i) => (
+                  el === correctAnswer
+                    ? (
+                      <Button
+                        btnClass={ changeClass ? 'right' : 'gray' }
+                        key={ i }
+                        btnName={ el }
+                        dataName="correct-answer"
+                        handleClick={ this.handleClick }
+                      />
+                    )
+                    : (
+                      <Button
+                        btnClass={ changeClass ? 'wrong' : 'gray' }
+                        key={ i }
+                        btnName={ el }
+                        dataName={ `wrong-answer-${i}` }
+                        handleClick={ this.handleClick }
+                      />
+                    )
+                ))}
+              </div>
+            </>
+          )}
+      </>
     );
   }
 }
-
 export default Game;
