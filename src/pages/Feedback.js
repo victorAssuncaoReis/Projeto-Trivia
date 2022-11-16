5f6643f33ec41029b35c47f996f8c0b3ca8669db
Leila Moraes <leila.violino@gmail.com>
2022-11-15 22:16:02 -0300

    feedback.css, algumas classes no feedback. timer.css

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -3,11 +3,18 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Button from '../components/Button';
+import LogoTrivia from '../images/LogoTrivia.png';
+import './feedback.css';
 
 class Feedback extends React.Component {
   state = {
     scoreCuttOff: 3,
+    changeClass: good,
   };
+
+  componentDidMount() {
+    this.changeClass();
+  }
 
   handleClickLogin = (route) => {
     window.location.replace(route);
@@ -18,26 +25,37 @@ class Feedback extends React.Component {
     history.push(route);
   };
 
+  changeClass = () => {
+    const { scoreCuttOff } = this.state;
+    const magicNumber = 3;
+    if (scoreCuttOff < magicNumber) {
+      this.setState({ changeClass: bad });
+    } this.setState({ changeClass: good });
+  };
+
   render() {
     const { assertions, score } = this.props;
-    const { scoreCuttOff } = this.state;
+    const { scoreCuttOff, changeClass } = this.state;
     return (
-      <div>
+      <div className="div-feed">
         <Header />
-        <h2 data-testid="feedback-text">
+        <img src={ LogoTrivia } alt="logo-trivia" />
+        <h2 className={ changeClass } data-testid="feedback-text">
           { assertions < scoreCuttOff ? 'Could be better...' : 'Well Done!'}
         </h2>
         <p data-testid="feedback-total-question">{assertions}</p>
         <p data-testid="feedback-total-score">{score}</p>
         <Button
+          btnClass="play-again"
           dataName="btn-play-again"
           handleClick={ () => this.handleClickLogin('/') }
           btnName="Jogar novamente"
         />
         <Button
+          btnClass="ranking"
           dataName="btn-ranking"
           handleClick={ () => this.handleClickRanking('/ranking') }
-          btnName="Ranking"
+          btnName="Ver Ranking"
         />
       </div>
     );
